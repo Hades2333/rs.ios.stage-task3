13f71f6af8e7b69f63c15022177d475c3e2c8bb8
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-05-23 13:41:51 +0300

    feat: implement task number 3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,62 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+
+    NSArray *arrayOfMonths = @[@"January", @"February", @"March", @"April", @"May", @"June", @"July", @"August", @"September",
+                               @"October", @"November", @"December"];
+
+    if (monthNumber > 0 && monthNumber < 13) {
+        NSString *answer = arrayOfMonths[monthNumber-1];
+        return answer;
+    } else {
+        return nil;
+    }
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+
+    NSDate *dateFromString = [formatter dateFromString:date];
+
+    NSCalendar * calendar = [NSCalendar currentCalendar];
+    NSDateComponents * dateComponents = [calendar components: NSCalendarUnitDay | NSCalendarUnitWeekday fromDate: dateFromString];
+
+    return (long)dateComponents.day;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+
+    NSCalendar * calendar = [NSCalendar currentCalendar];
+    NSDateComponents * dateComponents = [calendar components: NSCalendarUnitDay | NSCalendarUnitWeekday fromDate: date];
+
+    if (dateComponents == nil) {
+        return nil;
+    }
+
+    calendar.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    return calendar.shortWeekdaySymbols[dateComponents.weekday -1];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+
+    NSCalendar *myCalendar = [NSCalendar currentCalendar];
+
+    NSInteger dateOfTheWeek = [myCalendar component:NSCalendarUnitWeekOfYear fromDate:date];
+    NSInteger currentWeek = [myCalendar component:NSCalendarUnitWeekOfYear fromDate:[NSDate now]];
+
+    if (dateOfTheWeek == currentWeek) {
+        return YES;
+    } else {
+        return NO;
+    }
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,100 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+
+    //MARK: first string
+    NSMutableArray *firstArray = [[NSMutableArray alloc] init];
+    NSUInteger length = string1.length;
+    unichar buffer[length+1];
+
+    [string1 getCharacters:buffer range:NSMakeRange(0, length)];
+
+    // перебираем и кидаем в array
+    for (int i = 0; i < length; i++) {
+        char current = buffer[i];
+        NSString *currentNumber = [[NSString alloc] initWithFormat:@"%c", current];
+        [firstArray addObject:currentNumber];
+    }
+
+    //MARK: second string
+    NSMutableArray *secondArray = [[NSMutableArray alloc] init];
+    NSUInteger secondLength = string2.length;
+    unichar secondBuffer[secondLength+1];
+
+    [string2 getCharacters:secondBuffer range:NSMakeRange(0, secondLength)];
+
+    // перебираем и кидаем в array
+    for (int i = 0; i < secondLength; i++) {
+        char secondCurrent = secondBuffer[i];
+        NSString *currentNumber = [[NSString alloc] initWithFormat:@"%c", secondCurrent];
+        [secondArray addObject:currentNumber];
+    }
+
+    NSMutableString *answer = [[NSMutableString alloc] init];
+
+    for (int i = 0; i < (string1.length < string2.length ? string1.length : string2.length); i++) {
+
+        // проверка что в одном из массив пусто
+        if ((!firstArray || !firstArray.count) || (!secondArray || !secondArray.count)) {
+
+            // определяем где именно не хватает
+            if (!firstArray || !firstArray.count) {
+                for (int k = 0; k < secondArray.count; k++) {
+                    [answer appendString:secondArray[k]];
+                }
+                break;
+            }
+            if (!secondArray || !secondArray.count) {
+                for (int k = 0; k < firstArray.count; k++) {
+                    [answer appendString:firstArray[k]];
+                }
+                break;
+            }
+        }
+
+        NSComparisonResult result = [firstArray[i] compare: secondArray[i]];
+
+        if (result == NSOrderedAscending) {
+            [answer appendString:firstArray[i]];
+            NSLog(@"append %@", firstArray[i]);
+            
+            [firstArray removeObjectAtIndex:i];
+
+            i -= 1;
+        }
+
+        if (result == NSOrderedDescending) {
+            [answer appendString:secondArray[i]];
+            NSLog(@"append %@", secondArray[i]);
+
+            [secondArray removeObjectAtIndex:i];
+
+            i -= 1;
+        }
+
+        if (result == NSOrderedSame) {
+            [answer appendString:firstArray[i]];
+            NSLog(@"append %@", firstArray[i]);
+
+//            //MARK: химичим
+//            if (firstArray.count >= 2) {
+//                NSComparisonResult result = [firstArray[i] compare: firstArray[i+1]];
+//                if (result == NSOrderedDescending) {
+//                    [firstArray addObject:firstArray[i]];
+//                    [firstArray removeObjectAtIndex:i];
+//                }
+//            }
+
+            [firstArray removeObjectAtIndex:i];
+
+            // как вариант
+//            [secondArray removeObjectAtIndex:i];
+
+            i -= 1;
+        }
+    }
+    
+    return [answer copy];
 }
 
 @end
